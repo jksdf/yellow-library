ba35be1db87c55cfe327bde6e14cb3f925198d4f
Norbert Slivka <noroslivka@gmail.com>
2016-10-27 04:51:58 +0200

    Fix multiplicity annotations for Loan.

--- Backend/src/main/java/cz/muni/fi/pa165/yellowlibrary/backend/entity/Loan.java
+++ Backend/src/main/java/cz/muni/fi/pa165/yellowlibrary/backend/entity/Loan.java
@@ -12,10 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -43,7 +40,7 @@ public class Loan {
   private String loanState;
 
   @NotNull
-  @ManyToMany
+  @ManyToOne
   private User user;
 
   @NotNull
