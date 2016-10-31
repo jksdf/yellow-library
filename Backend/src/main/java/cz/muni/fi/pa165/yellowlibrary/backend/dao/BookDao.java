54d622b8aaab0d5e1f4635d0d58489283d358c34
Norbert Slivka <noroslivka@gmail.com>
2016-10-31 19:00:12 +0100

    Throws annotations in javadocs for book DAO.

--- Backend/src/main/java/cz/muni/fi/pa165/yellowlibrary/backend/dao/BookDao.java
+++ Backend/src/main/java/cz/muni/fi/pa165/yellowlibrary/backend/dao/BookDao.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pa165.yellowlibrary.backend.dao;
 
 import java.util.List;
 
+import javax.validation.ConstraintViolationException;
+
 import cz.muni.fi.pa165.yellowlibrary.backend.entity.Book;
 
 /**
@@ -12,6 +14,7 @@ public interface BookDao {
   /**
    * Retrieve the book.
    * @param id of the book requested.
+   * @return retrieved book or {@code null}, if there is no book with the ID.
    */
   Book getBookFromId(long id);
 
@@ -23,18 +26,25 @@ public interface BookDao {
   /**
    * Remove the book.
    * @param book to be removed.
+   * @throws NullPointerException if the {@code book} parameter is {@code null}.
    */
   void remove(Book book);
 
   /**
    * Stores new book.
    * @param book to be saved.
+   * @throws NullPointerException if the {@code book} is {@code null}.
+   * @throws ConstraintViolationException if {@code book} has {@code null} department, name,
+   *         number of pages or negative number of pages.
    */
   void create(Book book);
 
   /**
-   * Updates the stored book (according to the id).
+   * Updates the stored book (according to the ID).
    * @param book Updated book.
+   * @throws NullPointerException if the {@code book} is {@code null}.
+   * @throws ConstraintViolationException if {@code book} has {@code null} department, name,
+   *         number of pages or negative number of pages.
    */
   void update(Book book);
 }
