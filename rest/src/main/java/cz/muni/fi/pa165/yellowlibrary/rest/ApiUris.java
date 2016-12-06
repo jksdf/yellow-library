6937250caab992b503e7ecc7e5385504d253d1aa
Norbert Slivka <noroslivka@gmail.com>
2016-12-06 21:02:53 +0100

    Create REST API module.

---
+++ rest/src/main/java/cz/muni/fi/pa165/yellowlibrary/rest/ApiUris.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.yellowlibrary.rest;
+
+/**
+ * @author Norbert Slivka
+ */
+public final class ApiUris {
+  public static final String ROOT_URI_BOOK = "/book";
+
+  private ApiUris() {
+    throw new AssertionError("This is not the class you are looking for");
+  }
+}
---
+++ rest/src/main/java/cz/muni/fi/pa165/yellowlibrary/rest/controllers/MainController.java
@@ -0,0 +1,38 @@
+package cz.muni.fi.pa165.yellowlibrary.rest.controllers;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.apache.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import cz.muni.fi.pa165.yellowlibrary.rest.ApiUris;
+
+
+/**
+ * @author Norbert Slivka
+ */
+@RestController
+public class MainController {
+
+  private static final Logger x = org.apache.log4j.Logger.getLogger(MainController.class);
+
+  /**
+   * Main entry of REST API
+   *
+   * @return resource to uri mapping
+   */
+  @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public final Map<String, String> getResources() {
+
+    ImmutableMap.Builder<String, String> resourcesMap = ImmutableMap.builder();
+
+    resourcesMap.put("book_uri", ApiUris.ROOT_URI_BOOK);
+
+    return resourcesMap.build();
+  }
+}
